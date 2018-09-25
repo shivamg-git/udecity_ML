9d5f1225cbbadf80d42ea0b45f1da183c5a68464
spiderOO7 <shingwl@yahoo.com>
2018-09-25 18:12:56 +0530

    k mean done

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -65,7 +65,9 @@ plt.show()
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
-
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=2, random_state=0).fit(finance_features)
+pred = kmeans.predict(finance_features)
 
 
 ### rename the "name" parameter when you change the number of features
